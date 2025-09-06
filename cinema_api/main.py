7414fc8210c87e74a2f3f97ce1add6677115acf7
mbalbaid7 <m.a.balbaid@gmail.com>
2025-09-06 22:30:27 +0300

    Update main.py

--- cinema_api/main.py
+++ cinema_api/main.py
@@ -1,23 +1,25 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+# استدعاء الراوترات من الباكيج
 from cinema_api.routers import movies, customers, revenue, filters
-from cinema_api import data_loader
 
-app = FastAPI(title="Cinema API - Modular")
+app = FastAPI(title="Cinema API")
 
-# CORS علشان Streamlit يقدر يتصل
+# تفعيل CORS عشان Streamlit يتصل
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], allow_credentials=True,
-    allow_methods=["*"], allow_headers=["*"],
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # تسجيل الـ Routers
-app.include_router(movies.router)
-app.include_router(customers.router)
-app.include_router(revenue.router)
-app.include_router(filters.router)
+app.include_router(movies.router, prefix="/movies", tags=["Movies"])
+app.include_router(customers.router, prefix="/customers", tags=["Customers"])
+app.include_router(revenue.router, prefix="/revenue", tags=["Revenue"])
+app.include_router(filters.router, prefix="/filter", tags=["Filters"])
 
 
 @app.get("/")
